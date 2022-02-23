58fcf0a17909ce6d88455fd888b1f8aa68386e79
rishabh singh <rishabhbhangu13@icloud.com>
2022-02-23 01:01:25 -0500

    correcting the upload

---
+++ Lab_1_2_rishabhSingh_c0788019_Android/app/src/main/java/com/example/lab_1_2_rishabhsingh_c0788019_android/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.lab_1_2_rishabhsingh_c0788019_android.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ Lab_1_2_rishabhSingh_c0788019_Android/app/src/main/java/com/example/lab_1_2_rishabhsingh_c0788019_android/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.lab_1_2_rishabhsingh_c0788019_android.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
